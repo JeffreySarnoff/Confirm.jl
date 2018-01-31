5af3dceee529e5325b2d341546256e8b96534a7f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-01-31 07:16:15 -0500

    @confirm

--- src/Confirmed.jl
+++ src/Confirmed.jl
@@ -1,1 +1,24 @@
+module Confirmed
 
+exports @confirm, confirms!
+
+if VERSION < v"0.7.0-"
+    macro isdefined(x)
+        :(isdefined(Symbol($x)))
+    end
+end
+
+
+if ((@isdefined CONFIRM) && CONFIRM) || (get(ENV, "JULIA_CONFIRMS", "false") == "true") 
+    macro confirm(expr)
+        @assert $expr
+    end
+else
+    macro confirm(expr)
+    end
+end
+
+
+
+
+end # Confirmed
