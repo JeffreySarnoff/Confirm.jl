aab94bffbee6b56e4ad66abd8c0badeed4b01f03
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-22 11:17:11 -0500

    Update Confirmed.jl

--- src/Confirmed.jl
+++ src/Confirmed.jl
@@ -6,10 +6,19 @@ if (VERSION >= v"0.7.0-")
    CONFIRMS = (isdefined CONFIRMS) && CONFIRMS) ||
               get(ENV, "JULIA_CONFIRMS", "false")
 else        
-   CONFIRMS = (isdefined(:CONFIRMS) && CONFIRMS) ||
+   CONFIRMS = (@isdefined(:CONFIRMS) && CONFIRMS) ||
               get(ENV, "JULIA_CONFIRMS", "false")
 end
 
+
+if (CONFIRMS) 
+    macro confirm(expr)
+        :(@assert $expr)
+    end
+else
+    macro confirm(expr)
+    end
+end
 
 
 
