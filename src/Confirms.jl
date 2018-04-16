46edd2403863845dd0a7a6d081ec0945545a4722
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-16 07:27:12 -0400

    Update Confirms.jl

--- src/Confirms.jl
+++ src/Confirms.jl
@@ -6,7 +6,7 @@ CONFIRMS = get(ENV, "JULIA_CONFIRMS", "false") == "true"
 
 if (CONFIRMS) 
     macro confirm(expr)
-        :(@assert esc($expr))
+        :(@assert :(esc($$expr)))
     end
 else
     macro confirm(expr)
