96acd052b09e38f7755b708398a3698d55e61d7c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-30 05:19:02 -0400

    Update Confirms.jl

--- src/Confirms.jl
+++ src/Confirms.jl
@@ -2,7 +2,7 @@ module Confirms
 
 export @confirm
 
-CONFIRMS = get(ENV, "JULIA_CONFIRMS", "false") == "true" 
+CONFIRMS = get(ENV, "JULIA_CONFIRMS", "false") === "true" 
 
 if (CONFIRMS) 
     macro confirm(expr)
